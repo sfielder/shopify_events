2e63989550765841a7d1f0ee77cf7296a7ce1407
Scott Fielder <scottkfielder@gmail.com>
2013-08-12 20:14:57 -0400

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,11 +10,19 @@ class HomeController < ApplicationController
   
   def index
      
-    #if !ShopifyWebhook.verify_webhooks == Webhook.all.count #{request.protocol}#{request.host}
+    
+    puts "&&&&&&&&&&&&&&& ShopifyWebhook.verify_webhooks #{ShopifyWebhook.verify_webhooks}"
+    puts "&&&&&&&&&&&&&&& Webhook.all.count #{Webhook.all.count}"
+     
+    if ShopifyWebhook.verify_webhooks != Webhook.all.count #{request.protocol}#{request.host}
+      
       Webhook.each do |wh|
         puts " WEBHOOK --------------- #{ShopifyAPI::Webhook.create(:address => "https://shopify-events.herokuapp.com/#{wh.address}/#{Mongoid::Multitenancy.current_tenant.id}/webhook.json", :format => "#{wh.format}", :topic => "#{wh.topic}").to_yaml}"
       end
-    #end
+    
+    else
+      
+    end
     
     puts "************ ShopifyWebhook.verify_webhooks #{ShopifyWebhook.verify_webhooks} " 
     @webhookcount = ShopifyWebhook.verify_webhooks
